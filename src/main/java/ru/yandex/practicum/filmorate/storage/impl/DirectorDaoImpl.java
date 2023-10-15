2c00c396470270050a518976a276f98f764acf41
MarychevDmitry <blackriwz@yandex.ru>
2023-10-15 22:54:46 +0300

    add-director feat: Добавлены Режиссеры (Director) API feat: GET /films/director/{directorId}?sortBy=[year,likes]       Возвращает список фильмов режиссера отсортированных по количеству лайков или году выпуска. feat: GET /directors - Список всех режиссёров feat: GET /directors/{id}- Получение режиссёра по id feat: POST /directors - Создание режиссёра feat: PUT /directors - Изменение режиссёра feat: DELETE /directors/{id}

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/DirectorDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/DirectorDaoImpl.java
@@ -1,23 +1,18 @@
 package ru.yandex.practicum.filmorate.storage.impl;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.mapper.DirectorMapper;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.dao.DirectorDao;
 import static ru.yandex.practicum.filmorate.storage.sqloperation.DirectorSqlOperation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import java.util.List;
 import java.util.Map;
 
-@Slf4j
 @Component("directorDaoImpl")
 @AllArgsConstructor
 public class DirectorDaoImpl implements DirectorDao {
@@ -29,7 +24,6 @@ public class DirectorDaoImpl implements DirectorDao {
         SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("director").usingGeneratedKeyColumns("id");
         director.setId(simpleJdbcInsert.executeAndReturnKey(Map.of("name", director.getName())).longValue());
-        log.info("В базу добавлен новый режиссёр с id - {}", director.getId());
         return director;
     }
 
@@ -38,25 +32,23 @@ public class DirectorDaoImpl implements DirectorDao {
         jdbcTemplate.update(UPDATE_DIRECTOR.getTitle(),
                 director.getName(),
                 director.getId());
-        log.info("Режиссёр обновлен. id - {}", director.getId());
         return director;
     }
 
     @Override
     public void deleteDirector(Long directorId) {
         jdbcTemplate.update(DELETE_DIRECTOR.getTitle(), directorId);
-        log.info("Режиссёр с id {} удален", directorId);
     }
 
     @Override
     public List<Director> getDirectorsList() {
-        return jdbcTemplate.query(GET_ALL_DIRECTORS.getTitle(), this::mapRowToDirector);
+        return jdbcTemplate.query(GET_ALL_DIRECTORS.getTitle(), new DirectorMapper());
     }
 
     @Override
     public Director getDirectorById(Long directorId) {
         return jdbcTemplate.queryForObject(GET_DIRECTOR_BY_ID.getTitle(),
-                this::mapRowToDirector, directorId);
+                new DirectorMapper(), directorId);
     }
 
     @Override
@@ -64,12 +56,12 @@ public class DirectorDaoImpl implements DirectorDao {
         if(jdbcTemplate.queryForList(GET_DIRECTOR_BY_FILM_ID.getTitle(), filmId).isEmpty()) {
             return List.of();
         }
-        return jdbcTemplate.query(GET_DIRECTOR_BY_FILM_ID.getTitle(), this::mapRowToDirector, filmId);
+        return jdbcTemplate.query(GET_DIRECTOR_BY_FILM_ID.getTitle(), new DirectorMapper(), filmId);
     }
 
     @Override
     public boolean checkDirectorExistInDb(Long id) {
-        return !jdbcTemplate.query(GET_DIRECTOR_BY_ID.getTitle(), this::mapRowToDirector, id).isEmpty();
+        return !jdbcTemplate.query(GET_DIRECTOR_BY_ID.getTitle(), new DirectorMapper(), id).isEmpty();
     }
 
     @Override
@@ -86,12 +78,5 @@ public class DirectorDaoImpl implements DirectorDao {
     @Override
     public void deleteDirectorsFromFilm(Long filmId) {
         jdbcTemplate.update(DELETE_DIRECTOR_FROM_FILM.getTitle(), filmId);
-    }
-
-    public Director mapRowToDirector(ResultSet resultSet, int row) throws SQLException {
-        return Director.builder()
-                .id(resultSet.getLong("id"))
-                .name(resultSet.getString("Name"))
-                .build();
     }
 }
