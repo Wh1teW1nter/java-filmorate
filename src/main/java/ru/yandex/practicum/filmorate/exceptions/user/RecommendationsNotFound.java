c72b889193cbaec95bcc616ffaf66a9bfcbf4dda
Fattakh <LostHaoses@yandex.ru>
2023-10-17 20:58:37 +0300

    complite

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/user/RecommendationsNotFound.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/user/RecommendationsNotFound.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions.user;
 
-public class RecommendationsNotFound extends RuntimeException{
+public class RecommendationsNotFound extends RuntimeException {
     public RecommendationsNotFound(String message) {
         super(message);
     }
